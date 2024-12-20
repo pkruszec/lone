175f583e187b5bf8585866077ee2e853cecea433
Paweł Kruszec <pakruszec@gmail.com>
2024-12-20 20:00:35 +0100

    Add numbers, characters and basic string literals to lexer

--- main.c
+++ main.c
@@ -91,13 +91,29 @@ typedef enum {
     TOKEN_EOF = 0,
     TOKEN_SYM,
     TOKEN_NUM,
+    TOKEN_STR,
+    TOKEN_CHAR,
     // Operators
     TOKEN_EQUAL,
     TOKEN_PLUS,
+    TOKEN_SEMICOLON,
+    TOKEN_ASSIGN,
+    TOKEN_COLON,
+    TOKEN_DOT,
+    TOKEN_POPEN,
+    TOKEN_PCLOSE,
+    TOKEN_COPEN,
+    TOKEN_CCLOSE,
+    TOKEN_SOPEN,
+    TOKEN_SCLOSE,
     // Keywords
     TOKEN_PROC,
     TOKEN_TYPE,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_VAR,
     // Meta-tokens
+    TOKEN_SOME_RAW,
     TOKEN_SOME_OP,
     TOKEN_UNSET,
     TOKEN_COUNT,
@@ -120,11 +136,24 @@ typedef struct {
 static const char *operators[TOKEN_COUNT] = {
     [TOKEN_EQUAL] = "==",
     [TOKEN_PLUS] = "+",
+    [TOKEN_SEMICOLON] = ";",
+    [TOKEN_COLON] = ":",
+    [TOKEN_ASSIGN] = "=",
+    [TOKEN_DOT] = ".",
+    [TOKEN_POPEN] = "(",
+    [TOKEN_PCLOSE] = ")",
+    [TOKEN_COPEN] = "{",
+    [TOKEN_CCLOSE] = "}",
+    [TOKEN_SOPEN] = "[",
+    [TOKEN_SCLOSE] = "]",
 };
 
 static const char *keywords[TOKEN_COUNT] = {
     [TOKEN_PROC] = "proc",
     [TOKEN_TYPE] = "type",
+    [TOKEN_IF] = "if",
+    [TOKEN_ELSE] = "else",
+    [TOKEN_VAR] = "var",
 };
 
 bool is_white_space(char c)
@@ -138,6 +167,11 @@ bool is_white_space(char c)
 bool is_end_of_line(char c)
 {
     return c == '\n' || c == '\r';
+}
+
+bool is_digit(char c)
+{
+    return c >= '0' && c <= '9';
 }
 
 bool is_operator(char *str, int count, char c)
@@ -154,9 +188,11 @@ bool is_operator(char *str, int count, char c)
 
 bool is_symbol(char *str, int count, char c)
 {
+    (void)str;
+    (void)count;
     if (c == '/') return false;
     if (is_white_space(c)) return false;
-    if (is_operator(str, count, c)) return false;
+    if (is_operator(0, 0, c)) return false;
     return true;
 }
 
@@ -190,19 +226,42 @@ const char *token_type(Token_Type t)
         case TOKEN_EOF: return "eof";
         case TOKEN_SYM: return "sym";
         case TOKEN_NUM: return "num";
-        case TOKEN_EQUAL:
-        case TOKEN_PLUS:
-            return operators[t];
-        case TOKEN_TYPE:
-        case TOKEN_PROC:
-            return keywords[t];
-        case TOKEN_UNSET:
-        case TOKEN_COUNT:
-        case TOKEN_SOME_OP:
-            return "<invalid>";
+        case TOKEN_STR: return "str";
+        case TOKEN_CHAR: return "char";
+        case TOKEN_UNSET: return "unset";
+        case TOKEN_SOME_OP: return "some_op";
+        case TOKEN_SOME_RAW: return "some_raw";
+        default:  break;
     }
-    // TODO: Keywords, operators
+   
+    if (operators[t] != NULL) return operators[t];
+    if (keywords[t] != NULL) return keywords[t];
+
     return "<invalid>";
+}
+
+void token_repr(char *buf, int max, Token tok)
+{
+    if (tok.type == TOKEN_SYM || tok.type == TOKEN_NUM) {
+        snprintf(buf, max, "`%.*s`", (int)tok.len, tok.data);
+        return;
+    }
+  
+    // TODO: When reporting strings and characters, print all control and non-ASCII characters
+    // as escape codes, in a way compatible with the source code.
+
+    if (tok.type == TOKEN_STR) {
+        snprintf(buf, max, "\"%.*s\"", (int)tok.len, tok.data);
+        return;
+    }
+
+    if (tok.type == TOKEN_CHAR) {
+        snprintf(buf, max, "'%.*s'", (int)tok.len, tok.data);
+        return;
+    }
+
+    // Fallback.
+    snprintf(buf, max, "`%s`", token_type(tok.type));
 }
 
 char lexer_advance(Lexer *lex)
@@ -230,6 +289,23 @@ char lexer_peek2(Lexer *lex)
 {
     if (lex->pos >= lex->len - 2) return 0;
     return lex->src[lex->pos + 1];
+}
+
+char lexer_escape(Lexer *lex)
+{
+    char c = lexer_peek(lex);
+    if (c == '\'') {
+        lexer_advance(lex);
+        return '\'';
+    } else if (c == '\"') {
+        lexer_advance(lex);
+        return '\"';
+    } else if (c == 'n') {
+        lexer_advance(lex);
+        return '\n';
+    }
+    // TODO: Support everything C supports
+    return 0;
 }
 
 void lexer_next(Lexer *lex, Token *tok)
@@ -295,11 +371,71 @@ void lexer_next(Lexer *lex, Token *tok)
                 sb.count = 0;
                 index = 0;
                 token_loc = lex->loc;
+                char n = lexer_peek2(lex);
+              
+                // TODO: Raw Strings
+                // ``test``
+                // `GLSL`// `funcer`test`GLSL`
+                //
 
-                if (is_operator(0, 0, c)) {
+                if (c == '\'') {
+                    type = TOKEN_CHAR;
+                    lexer_advance(lex);
+                } else if (c == '\"') {
+                    type = TOKEN_STR;
+                    lexer_advance(lex);
+                } else if (is_digit(c) || (c == '.' && is_digit(n))) {
+                    type = TOKEN_NUM;
+                } else if (is_operator(0, 0, c)) {
                     type = TOKEN_SOME_OP;
                 } else {
                     type = TOKEN_SYM;
+                }
+            } break;
+            case TOKEN_STR: {
+                if (c == '\\') {
+                    lexer_advance(lex);
+                    char e = lexer_escape(lex);
+                    if (e == 0) {
+                        char bad = lexer_peek(lex);
+                        if (bad != 0)
+                            error_exit(token_loc, "Unrecognized escape sequence `\\%c`.", bad);
+                        else
+                            error_exit(token_loc, "Expected an escape sequence, got EOF.");
+                    }
+                    sb_append(&sb, e);
+                } else if (c == '\"') {
+                    tok->type = type;
+                    tok->len = sb.count;
+                    tok->data = sb.data;
+                    lexer_advance(lex);
+                    return;
+                } else {
+                    sb_append(&sb, c);
+                    lexer_advance(lex);
+                }
+            } break;
+            case TOKEN_CHAR: {
+                if (c == '\\') {
+                    lexer_advance(lex);
+                    char e = lexer_escape(lex);
+                    if (e == 0) {
+                        char bad = lexer_peek(lex);
+                        if (bad != 0)
+                            error_exit(token_loc, "Unrecognized escape sequence `\\%c`.", bad);
+                        else
+                            error_exit(token_loc, "Expected an escape sequence, got EOF.");
+                    }
+                    sb_append(&sb, e);
+                } else if (c == '\'') {
+                    tok->type = type;
+                    tok->len = sb.count;
+                    tok->data = sb.data;
+                    lexer_advance(lex);
+                    return;
+                } else {
+                    sb_append(&sb, c);
+                    lexer_advance(lex);
                 }
             } break;
             case TOKEN_SOME_OP: {
@@ -334,13 +470,27 @@ void lexer_next(Lexer *lex, Token *tok)
                     return;
                 }
             } break;
+            case TOKEN_NUM: {
+                // We use is_symbol here, because at the lexing stage we don't care about the correctness
+                // of the number, we just want to pass it down.
+                if (c == '.' || is_symbol(sb.data, sb.count, c)) {
+                    sb_append(&sb, c);
+                    lexer_advance(lex);
+                } else {
+                    tok->loc = token_loc;
+                    tok->type = type;
+                    tok->len = sb.count;
+                    tok->data = sb.data;
+                    return;
+                }
+            } break;
             default: {
-                error_exit(lex->loc, "Unimplemented token type.");
+                error_exit(lex->loc, "Unimplemented token type `%s`", token_type(type));
             } break;
         }
     }
 
-    tok->type = TOKEN_EOF;    
+    tok->type = TOKEN_EOF; 
 }
 
 int main(void)
@@ -361,7 +511,9 @@ int main(void)
     while (1) {
         Token tok = {0};
         lexer_next(&lex, &tok);
-        printf("%s\n", token_type(tok.type));
+        char buf[64] = {0};
+        token_repr(buf, ARRAY_COUNT(buf) - 1, tok);
+        printf("%s(%s)\n", token_type(tok.type), buf); 
         if (tok.type == TOKEN_EOF) break;
     }
      
